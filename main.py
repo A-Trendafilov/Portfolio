b97d0f6e8b7657c7c0b6318e5cfee9ea0c99e1fd
Angel Trendafilov <aatrendafilov@gmail.com>
2024-04-25 18:11:52 +0300

    Fix age calculation.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
 from datetime import datetime
-from flask import Flask, render_template
+
 from dotenv import load_dotenv
+from flask import Flask, render_template
 
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
@@ -10,11 +11,27 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv(SECRET_KEY)
 
 
+def calculate_age(birthday_date):
+    current_date = datetime.now()
+    age = (
+        current_date.year
+        - birthday_date.year
+        - (
+            (current_date.month, current_date.day)
+            < (birthday_date.month, birthday_date.day)
+        )
+    )
+    return age
+
+
+birthday = datetime(1987, 6, 22)
+
+
 @app.route("/")
 def home():
-    current_year = int(datetime.now().strftime("%Y"))
-    return render_template("index.html", current_year=current_year)
+    age = calculate_age(birthday)
+    return render_template("index.html", current_age=age)
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
