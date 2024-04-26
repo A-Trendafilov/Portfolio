e7c2eaceffa5ccd405cd518424c5c367f8fd1506
Angel Trendafilov <aatrendafilov@gmail.com>
2024-04-26 13:11:58 +0300

    Add contact function.

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import os
+import smtplib
 from datetime import datetime
 
 from dotenv import load_dotenv
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
+EMAIL = os.getenv("EMAIL")
+PASSWORD = os.getenv("PASSWORD")
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv(SECRET_KEY)
+birthday = datetime(1987, 6, 22)
 
 
 def calculate_age(birthday_date):
@@ -24,13 +28,37 @@ def calculate_age(birthday_date):
     return age
 
 
-birthday = datetime(1987, 6, 22)
+def send_email(name, email, subject, message):
+    email_message = (
+        f"Subject:{subject}\n\nName: {name}\nEmail: {email}\nMessage:{message}"
+    )
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(EMAIL, PASSWORD)
+        connection.sendmail(
+            from_addr=EMAIL,
+            to_addrs=EMAIL,
+            msg=email_message.encode("utf-8"),
+        )
 
 
 @app.route("/")
 def home():
     age = calculate_age(birthday)
     return render_template("index.html", current_age=age)
+
+
+@app.route("/contact", methods=["POST"])
+def contact():
+    if request.method == "POST":
+        form_data = request.form
+        send_email(
+            form_data["name"],
+            form_data["email"],
+            form_data["subject"],
+            form_data["message"],
+        )
+        return "OK"
 
 
 if __name__ == "__main__":
---
+++ static/assets/vendor/contact-form/validate.js
@@ -0,0 +1,57 @@
+document.addEventListener("DOMContentLoaded", function() {
+  let forms = document.querySelectorAll('.php-email-form');
+
+  forms.forEach(function(form) {
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+
+      let thisForm = this;
+
+      let action = thisForm.getAttribute('action');
+
+      if (!action) {
+        displayError(thisForm, 'The form action property is not set!');
+        return;
+      }
+      thisForm.querySelector('.loading').classList.add('d-block');
+      thisForm.querySelector('.error-message').classList.remove('d-block');
+      thisForm.querySelector('.sent-message').classList.remove('d-block');
+
+      let formData = new FormData(thisForm);
+
+      // Make a fetch request to your Flask server
+      fetch(action, {
+          method: 'POST',
+          body: formData,
+          headers: {
+            'X-Requested-With': 'XMLHttpRequest'
+          }
+        })
+        .then(response => {
+          if (response.ok) {
+            return response.text();
+          } else {
+            throw new Error(`${response.status} ${response.statusText} ${response.url}`);
+          }
+        })
+        .then(data => {
+          thisForm.querySelector('.loading').classList.remove('d-block');
+          if (data.trim() == 'OK') {
+            thisForm.querySelector('.sent-message').classList.add('d-block');
+            thisForm.reset();
+          } else {
+            displayError(thisForm, data ? data : 'Form submission failed and no error message returned from the server');
+          }
+        })
+        .catch((error) => {
+          displayError(thisForm, error.message);
+        });
+    });
+  });
+
+  function displayError(thisForm, error) {
+    thisForm.querySelector('.loading').classList.remove('d-block');
+    thisForm.querySelector('.error-message').innerHTML = error;
+    thisForm.querySelector('.error-message').classList.add('d-block');
+  }
+});
